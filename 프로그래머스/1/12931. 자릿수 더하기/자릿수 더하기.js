13f40db05706140bd0dce0620225aa235a70dae7
Chaeni Lee <157700894+chaenilee@users.noreply.github.com>
2024-05-14 23:23:22 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,10 +1,3 @@
 function solution(n){
-    let cnt = 0;
-    let arr = (n + "").split("");
-    
-    for(let i = 0; i < arr.length; i++){
-        cnt += Number(arr[i]);
-    }
-
-    return cnt;
+    return (n + "").split("").reduce((a, b) => Number(a) + Number(b), 0);
 }
