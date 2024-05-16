73f12a2139af144e7496515ee732349acbaa5063
Chaeni Lee <157700894+chaenilee@users.noreply.github.com>
2024-05-17 03:18:24 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 2.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,15 @@
+function solution(num_list) {
+    let evenSum = 0;
+    let oddSum = 0;
+    
+    for(let i = 0; i < num_list.length; i++){
+        if(i % 2 === 0){
+            evenSum += num_list[i];            
+        } else {
+            oddSum += num_list[i];
+        }
+    }
+    console.log(oddSum, evenSum)
+    
+    return oddSum >= evenSum ? oddSum : evenSum;
+}
