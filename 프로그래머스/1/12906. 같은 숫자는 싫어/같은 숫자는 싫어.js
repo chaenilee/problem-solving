f574cef184ab5fe2bf3023431263c982defdffcf
Chaeni Lee <157700894+chaenilee@users.noreply.github.com>
2024-05-16 14:59:45 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 15.52 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,10 @@
+function solution(arr){
+    let stack = [];
+    
+    for(let i = 0; i < arr.length; i++){
+        stack.push(arr[i]);
+        if(arr[i] === arr[i + 1]) stack.pop();
+    }
+    
+    return stack;
+}
