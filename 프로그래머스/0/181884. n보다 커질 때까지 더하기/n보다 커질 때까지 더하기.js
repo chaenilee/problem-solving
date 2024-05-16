124ff848f16543c68489caf4bac14d966c357b49
Chaeni Lee <157700894+chaenilee@users.noreply.github.com>
2024-05-17 04:53:25 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -1,9 +1,9 @@
 function solution(numbers, n) {
     let cnt = 0;
+    let i = 0;
     
-    for(let i = 0; i < numbers.length; i++){
-        if(cnt > n) break;
-        cnt += numbers[i];
+    while(cnt <= n){
+        cnt += numbers[i++]
     }
     
     return cnt;
