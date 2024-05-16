e8077f7e26d3d33922f54c4fb91639015ea46a7f
Chaeni Lee <157700894+chaenilee@users.noreply.github.com>
2024-05-17 02:54:12 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,8 +1,3 @@
 function solution(strlist) {
-    let answer = [];
-    
-    for(let i = 0; i < strlist.length; i++){
-        answer.push(strlist[i].length)
-    }
-    return answer;
+    return strlist.map(x => x.length)
 }
