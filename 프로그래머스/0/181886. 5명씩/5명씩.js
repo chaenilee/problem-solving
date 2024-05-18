ac08d28a09e0937a3565439eba792dedb0800a36
Chaeni Lee <157700894+chaenilee@users.noreply.github.com>
2024-05-18 20:09:59 +0900

    [level 0] Title: 5명씩, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.js
@@ -0,0 +1,8 @@
+function solution(names) {
+    var answer = [names[0]];
+    
+    for(let i = 1; i < names.length; i++){
+        if(i % 5 === 0) answer.push(names[i])
+    }
+    return answer;
+}
