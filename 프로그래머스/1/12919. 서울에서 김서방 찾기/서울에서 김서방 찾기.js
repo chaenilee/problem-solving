37dcec3286551af317fc552295c81c7d424aadec
Chaeni Lee <157700894+chaenilee@users.noreply.github.com>
2024-05-19 22:21:00 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,7 +1,5 @@
 function solution(seoul) {
-    for(let i = 0; i < seoul.length; i++){
-        if(seoul[i] === "Kim"){
-            return "김서방은 " + i + "에 있다"
-        }
-    }
+    let idx = seoul.indexOf("Kim");
+    return "김서방은 " + idx + "에 있다"
+
 }
