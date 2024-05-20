32d69ec5e7988a48fe09b21360a3fd9f152aa437
Chaeni Lee <157700894+chaenilee@users.noreply.github.com>
2024-05-20 23:45:50 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    let answer = [n];
+    
+    
+    while(n !== 1){
+        
+        if(n % 2 === 0){
+            answer.push(n /= 2);
+        }
+        else{
+            answer.push(n = 3 * n + 1);
+        }
+    }
+    
+    return answer;
+}
