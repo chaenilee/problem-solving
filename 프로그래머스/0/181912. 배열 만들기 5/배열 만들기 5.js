a505691da50ff93bc04b029249d75f3adf7b0780
Chaeni Lee <157700894+chaenilee@users.noreply.github.com>
2024-05-21 23:31:51 +0900

    [level 0] Title: 배열 만들기 5, Time: 2.24 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
@@ -2,7 +2,7 @@ function solution(intStrs, k, s, l) {
     var answer = [];
     
     for(let i = 0; i < intStrs.length; i++){
-        answer.push(parseInt(intStrs[i].slice(s, s+l)));
+        answer.push(+intStrs[i].slice(s, s+l));
     }
     
     return answer.filter(int => int > k);
