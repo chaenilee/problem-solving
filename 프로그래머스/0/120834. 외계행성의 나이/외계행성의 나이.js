7cc444c69d69ef841fb9a3ebbf382541443f332f
Chaeni Lee <157700894+chaenilee@users.noreply.github.com>
2024-05-21 23:00:56 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,11 +1,7 @@
 function solution(age) {
-    var answer = '';
-    const alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
-    let ageArr = (age+"").split('');
-    
-    for(let i = 0; i < ageArr.length; i++){
-        answer += alpha[Number(ageArr[i])];
-    }
-    
-    return answer;
+    return age
+        .toString()
+        .split('')
+        .map(x => "abcdefghij"[x])
+        .join('');
 }
