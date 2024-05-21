fe460c3ee822ee41eba2d9e5a030e3bebe5a5ef9
Chaeni Lee <157700894+chaenilee@users.noreply.github.com>
2024-05-21 19:03:16 +0900

    [level 0] Title: 약수 구하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -2,8 +2,8 @@ function solution(n) {
     var answer = [];
     
     for(let i = 1; i <= n; i++){
-        if(Number.isInteger(n / i)) answer.push(i)
+        if(n%i ===0) answer.push(i)
     }
     
-    return answer.sort((a, b) => a - b);
+    return answer;
 }
