56cf085aa273fabececad3b19eb4334e18db187e
Chaeni Lee <157700894+chaenilee@users.noreply.github.com>
2024-05-22 09:42:05 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 4.64 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -1,15 +1,7 @@
 function solution(arr) {
-    var answer = [];
-    
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] >= 50 && arr[i] % 2 === 0){
-            answer.push(arr[i]/2);
-        } else if(arr[i] < 50 && arr[i] % 2 !== 0){
-            answer.push(arr[i] * 2);
-        } else {
-            answer.push(arr[i])
-        }
-    }   
-    
-    return answer;
+    return arr.map(num => {
+        if(num >= 50 && !(num % 2)) return num / 2;
+        else if(num < 50 && num % 2) return num * 2;
+        return num;
+    });
 }
