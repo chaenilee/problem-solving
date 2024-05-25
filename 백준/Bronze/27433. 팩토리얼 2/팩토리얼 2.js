08e258332918742b3b8928cdf089e0a0a428fbcb
Chaeni Lee <chaenilee28@gmail.com>
2024-05-26 00:11:05 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 120 ms, Memory: 9600 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.js
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.js
@@ -1,11 +1,10 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
-//let input = require('fs').readFileSync('example.txt').toString().split('\n');
-let a = parseInt(input[0]);
-let b = parseInt(input[1]);
+// let input = require('fs').readFileSync('example.txt').toString().split('\n');
+let a = parseInt(input);
 
 function fact(n){
     if(n <= 1) return 1;
     return n * fact(n - 1);
 }
 
-console.log(fact(input));
+console.log(fact(a));
