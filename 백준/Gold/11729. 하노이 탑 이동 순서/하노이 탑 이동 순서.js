15fe8862467d02297b684462097a95a07b237c3b
Chaeni Lee <chaenilee28@gmail.com>
2024-06-03 09:44:03 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 600 ms, Memory: 155764 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.js
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.js
@@ -1,19 +1,30 @@
-const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
 
-answer = "";
-const recursion = (n, from, temp, to) => {
-  if (n === 1) {
-    answer += `${from} ${to}\n`;
-    return;
-  }
-  recursion(n - 1, from, to, temp);
-  answer += `${from} ${to}\n`;
-  recursion(n - 1, temp, from, to);
+const solution = (input) => {
+  let callHanoiCnt = 0;
+  let paths = [];
+
+  const hanoi = (disk, from, to) => {
+    let via = 6 - (from + to);
+
+    if (disk === 1) {
+      callHanoiCnt ++;
+      paths.push(`${from} ${to}`);
+      return;
+    }
+
+    hanoi(disk - 1, from, via);
+    // cnt를 1로 재호출한 것 very interesting
+    // 그치만 호출보다 직접 path.push 방법이 좋을 것 같다
+    hanoi(1, from, to);
+    hanoi(disk - 1, via, to);
+  };
+
+  hanoi(input, 1, 3);
+
+  return `${callHanoiCnt}\n${paths.join("\n")}`;
 };
 
-const hanoiTop = (n, from, temp, to) => {
-  console.log(2 ** n - 1);
-  recursion(n, from, temp, to);
-};
-hanoiTop(Number(input), 1, 2, 3);
+const answer = solution(Number(input));
 console.log(answer);
