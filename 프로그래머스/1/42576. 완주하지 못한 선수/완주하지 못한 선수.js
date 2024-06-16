68709be2b984789884b8971d4dd6e564d3b9139c
Chaeni Lee <chaenilee28@gmail.com>
2024-06-17 08:57:53 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 92.79 ms, Memory: 56.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,11 @@
+function solution(part, comp) {
+    part.sort();
+    comp.sort();
+    
+    for (let i = 0; i < part.length; i++) {
+        if (part[i] !== comp[i]) {
+            return part[i];
+        }
+    }
+    return part[part.length - 1];
+}
