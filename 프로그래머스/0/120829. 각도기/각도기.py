6d47734322dfdfeab45e53ec937211f34fff77e7
Chaeni Lee <chaenilee28@gmail.com>
2024-10-29 09:05:31 -0700

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.py
+++ 프로그래머스/0/120829. 각도기/각도기.py
@@ -1,9 +1,5 @@
 def solution(angle):
-    if angle < 90:
-        return 1
-    elif angle == 90:
-        return 2
-    elif angle > 90 and angle < 180:
-        return 3
-    else:
-        return 4
+    if angle <= 90:
+        return 1 if angle < 90 else 2
+    else: 
+        return 3 if angle < 180 else 4
